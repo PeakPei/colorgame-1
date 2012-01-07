5a11b8cc3c1a05dfd214f1eb5455666cd1a99fdf
simeonlu <simeonlu@gmail.com>
2012-01-07 21:16:22 +0000

    Initial Commit

---
+++ ColorGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColorGame
+//
+//  Created by shimin lu on 07/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ColorGame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ColorGame
+//
+//  Created by shimin lu on 07/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
